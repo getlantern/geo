5b87700e3d543fde47f7696a8e5892b16cb0e1a3
Joesis <joesis@getlantern.org>
2020-07-16 03:55:57 -0700

    support look up ISP

--- geo.go
+++ geo.go
@@ -1,9 +1,11 @@
-// Package geo provides functionality for looking up country codes based on
-// IP addresses.
+// Package geo provides functionality for looking up country code and ISP name
+// of the given IP address.
 package geo
 
 import (
+	"io/ioutil"
 	"net"
+	"os"
 	"sync/atomic"
 	"time"
 
@@ -16,17 +18,29 @@ var (
 	log = golog.LoggerFor("geo")
 )
 
-// Lookup allows looking up the country for an IP address
 type Lookup interface {
+	CountryLookup
+	ISPLookup
+}
+
+// CountryLookup allows looking up the country for an IP address
+type CountryLookup interface {
 	// CountryCode looks up the 2 digit ISO 3166 country code in upper case for
 	// the given IP address and returns "" if there was an error.
 	CountryCode(ip net.IP) string
 }
 
-// NoLookup is a Lookup implementation which always return empty country code.
+// ISPLookup allows looking up the ISP for an IP address
+type ISPLookup interface {
+	// ISP looks up the ISP name for the given IP address and returns "" if there was an error.
+	ISP(ip net.IP) string
+}
+
+// NoLookup is a Lookup implementation which always return empty result.
 type NoLookup struct{}
 
 func (l NoLookup) CountryCode(ip net.IP) string { return "" }
+func (l NoLookup) ISP(ip net.IP) string         { return "" }
 
 type lookup struct {
 	runner *keepcurrent.Runner
@@ -37,8 +51,13 @@ type lookup struct {
 // fetched from the given URL and keeps in sync with it every syncInterval. If filePath
 // is not empty, it saves the database file to filePath and uses the file if
 // available.
-func New(dbURL string, syncInterval time.Duration, filePath string) Lookup {
-	source := keepcurrent.FromTarGz(keepcurrent.FromWeb(dbURL), "GeoLite2-Country.mmdb")
+func New(dbURL string, syncInterval time.Duration, filePath string) *lookup {
+	return FromWeb(dbURL, "GeoLite2-Country.mmdb", syncInterval, filePath)
+}
+
+// FromWeb is same as New but allows downloading a different MaxMind database
+func FromWeb(dbURL string, nameInTarball string, syncInterval time.Duration, filePath string) *lookup {
+	source := keepcurrent.FromTarGz(keepcurrent.FromWeb(dbURL), nameInTarball)
 	chDB := make(chan []byte)
 	dest := keepcurrent.ToChannel(chDB)
 	var runner *keepcurrent.Runner
@@ -70,6 +89,26 @@ func New(dbURL string, syncInterval time.Duration, filePath string) Lookup {
 	return v
 }
 
+// FromFile uses the local database file for lookup
+func FromFile(filePath string) (*lookup, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	db, err := geoip2.FromBytes(data)
+	if err != nil {
+		return nil, err
+	}
+	v := &lookup{}
+	v.db.Store(db)
+
+	return v, nil
+}
+
 func (l *lookup) CountryCode(ip net.IP) string {
 	if db := l.db.Load(); db != nil {
 		geoData, err := db.(*geoip2.Reader).Country(ip)
@@ -78,6 +117,18 @@ func (l *lookup) CountryCode(ip net.IP) string {
 			return ""
 		}
 		return geoData.Country.IsoCode
+	}
+	return ""
+}
+
+func (l *lookup) ISP(ip net.IP) string {
+	if db := l.db.Load(); db != nil {
+		geoData, err := db.(*geoip2.Reader).ISP(ip)
+		if err != nil {
+			log.Debugf("Unable to look up ip address %s: %s", ip, err)
+			return ""
+		}
+		return geoData.ISP
 	}
 	return ""
 }
